1cbc27e9f25bb9005e7e5d6c4ece8799e09d6d58
Wiktor <wiktor.muraszko@wp.pl>
2020-12-17 19:13:50 +0100

    small changes

--- Start.py
+++ Start.py
@@ -3,17 +3,18 @@ import cv2
 import numpy as np
 
 face_detector = cv2.CascadeClassifier('xml_detectors\\haarcascades\\haarcascade_frontalface_default.xml')
-eye_detector = cv2.CascadeClassifier('xml_detectors\\haarcascades\\haarcascade_eye.xml')
+eye_detector = cv2.CascadeClassifier('xml_detectors\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml')
 
-img = cv2.imread('Data/CV_photo.jpg', cv2.IMREAD_UNCHANGED)
-
+img_open = cv2.imread('Data\\IMG_20201217_184415.jpg', cv2.IMREAD_UNCHANGED)
+img_closed = cv2.imread('Data\\IMG_20201216_185903.jpg', cv2.IMREAD_UNCHANGED)
+img = img_open
 print("Size : ", img.shape)
-scale_percen = 40
-n_width = int(img.shape[1] * scale_percen / 100)
-n_heigh = int(img.shape[0] * scale_percen / 100)
+scale_percent = 600 / img.shape[1]
+n_width = int(img.shape[1] * scale_percent)
+n_heigh = int(img.shape[0] * scale_percent)
 n_dim = (n_width, n_heigh)
 
-resized_img = cv2.resize(img, n_dim, interpolation= cv2.INTER_AREA)
+resized_img = cv2.resize(img, n_dim, interpolation=cv2.INTER_AREA)
 
 print("New Size : ", resized_img.shape)
 
@@ -23,7 +24,7 @@ cv2.destroyAllWindows()
 
 gray_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
 print("Begining Detection")
-face = face_detector.detectMultiScale(gray_img, 1.3, 5)
+face = face_detector.detectMultiScale(gray_img, 1.2, 5)
 print("Ending Detection")
 if face is ():
     print("NO FACE DETECTED")
@@ -32,7 +33,7 @@ for (x, y, w, h) in face:
     cv2.rectangle(resized_img, (x, y), (x + w, y + h), (127, 0, 255), 2)
     roi_grey = gray_img[y:y+h, x:x+w]
     roi_color = resized_img[y:y+h, x:x+w]
-    eyes = eye_detector.detectMultiScale(roi_grey)
+    eyes = eye_detector.detectMultiScale(roi_grey, 1.05, 3)
     for (ex, ey, ew, eh) in eyes:
         cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
