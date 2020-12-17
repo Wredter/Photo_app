21b032f90fb9d0d13c11c5d731e2f65e75a896c6
Wiktor <wiktor.muraszko@wp.pl>
2020-12-17 19:48:11 +0100

    hyperparameters changes may need some fine tuning.

--- Start.py
+++ Start.py
@@ -3,11 +3,17 @@ import cv2
 import numpy as np
 
 face_detector = cv2.CascadeClassifier('xml_detectors\\haarcascades\\haarcascade_frontalface_default.xml')
-eye_detector = cv2.CascadeClassifier('xml_detectors\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml')
+eye_detector_1 = cv2.CascadeClassifier('xml_detectors\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml')
+eye_detector_2 = cv2.CascadeClassifier('xml_detectors\\haarcascades\\haarcascade_eye.xml')
+eye_detector_3 = cv2.CascadeClassifier('xml_detectors\\haarcascades\\haarcascade_lefteye_2splits.xml')
+smile_detector = cv2.CascadeClassifier('xml_detectors\\haarcascades\\haarcascade_smile.xml')
 
 img_open = cv2.imread('Data\\IMG_20201217_184415.jpg', cv2.IMREAD_UNCHANGED)
 img_closed = cv2.imread('Data\\IMG_20201216_185903.jpg', cv2.IMREAD_UNCHANGED)
+
 img = img_open
+eye_detector = eye_detector_1
+
 print("Size : ", img.shape)
 scale_percent = 600 / img.shape[1]
 n_width = int(img.shape[1] * scale_percent)
@@ -33,9 +39,12 @@ for (x, y, w, h) in face:
     cv2.rectangle(resized_img, (x, y), (x + w, y + h), (127, 0, 255), 2)
     roi_grey = gray_img[y:y+h, x:x+w]
     roi_color = resized_img[y:y+h, x:x+w]
-    eyes = eye_detector.detectMultiScale(roi_grey, 1.05, 3)
+    eyes = eye_detector.detectMultiScale(roi_grey, 1.05, 4)
+    smile = smile_detector.detectMultiScale(roi_grey, 1.2, 6)
     for (ex, ey, ew, eh) in eyes:
         cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+    for (sx, sy, sw, sh) in smile:
+        cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (255, 255, 20), 2)
 
 cv2.imshow('Face Detection', resized_img)
 cv2.waitKey(0)
